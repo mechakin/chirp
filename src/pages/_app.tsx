f51a9b294bd94f4570913c45a1b553c812bf09a7
mechakin <barrientosryan83@gmail.com>
2023-03-31 23:26:07 -0400

    Posting loading states and add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,21 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
       <Component {...pageProps} />
     </ClerkProvider>
   );
