b9425cf13007744c3cce9f79fd8d518ca8098419
mechakin <ryan.barrientos765@gmail.com>
2023-04-02 03:13:26 -0400

    seperate ssg helper, full post page, edit ci

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -65,6 +65,19 @@ export const postsRouter = createTRPCRouter({
     return addUserDataToPosts(posts);
   }),
 
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.findUnique({
+        where: { id: input.id },
+      });
+
+      if (!post)
+        throw new TRPCError({ code: "NOT_FOUND", message: "Posts not found." });
+
+      return (await addUserDataToPosts([post]))[0];
+    }),
+
   create: privateProcedure
     .input(
       z.object({
