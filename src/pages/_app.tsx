64d656a73125b6ff90ddad7c086dab67ca7b2fa8
mechakin <ryan.barrientos765@gmail.com>
2023-04-01 20:26:12 -0400

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
-
+import Head from "next/head";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
@@ -9,6 +9,11 @@ import { Toaster } from "react-hot-toast";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="Twitter but it's only emojis" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster
         position="bottom-center"
         toastOptions={{
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,4 @@
-import type { User } from "@clerk/nextjs/dist/api";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -24,14 +24,6 @@ const ratelimit = new Ratelimit({
    */
   prefix: "@upstash/ratelimit",
 });
-
-function filterUserForClient(user: User) {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-}
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
