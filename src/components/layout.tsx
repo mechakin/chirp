40acbd28625d909000c15acabb78fc5adc3a2b60
mechakin <ryan.barrientos765@gmail.com>
2023-04-01 20:53:46 -0400

    Render posts on profile page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { type PropsWithChildren } from "react";
 
-export default function PageLayout(props: PropsWithChildren) {
+export function PageLayout(props: PropsWithChildren) {
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full overflow-y-auto border-x border-slate-400 md:max-w-2xl">
