a73502b74e63d2d66690c55f6d036b1bf88b002e
mechakin <barrientosryan83@gmail.com>
2023-05-26 03:53:54 -0400

    revamp website

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -1,15 +1,30 @@
-import { type RouterOutputs } from "~/utils/api";
+import { api, type RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 
 dayjs.extend(relativeTime);
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+type PostWithUser = RouterOutputs["posts"]["infiniteFeed"]["posts"][number];
 
 export function PostView(props: PostWithUser) {
+  const trpcUtils = api.useContext();
+  const toggleLike = api.posts.toggleLike.useMutation({
+    onSuccess: async () => {
+      await trpcUtils.posts.infiniteFeed.invalidate();
+      await trpcUtils.posts.infiniteProfileFeed.invalidate();
+    },
+  });
+
   const { post, author } = props;
+
+  function handleToggleLike() {
+    toggleLike.mutate({ id: post.id });
+  }
+
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
       <Link href={`/@${author.username}`}>
@@ -21,7 +36,7 @@ export function PostView(props: PostWithUser) {
           height={56}
         />
       </Link>
-      {/* might not need the flex stuff here */}
+
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>
@@ -36,7 +51,62 @@ export function PostView(props: PostWithUser) {
           </span>
         </div>
         <span className="text-2xl">{post.content}</span>
+        <HeartButton
+          onClick={handleToggleLike}
+          isLoading={toggleLike.isLoading}
+          likedByMe={post.likedByMe}
+          likeCount={post.likeCount}
+        />
       </div>
     </div>
+  );
+}
+
+type HeartButtonProps = {
+  onClick: () => void;
+  isLoading: boolean;
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HeartButton({
+  isLoading,
+  onClick,
+  likedByMe,
+  likeCount,
+}: HeartButtonProps) {
+  const { isSignedIn } = useUser();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (!isSignedIn) {
+    return (
+      <div className={`mb-1 mt-1 flex items-center gap-3 self-start text-gray-500`}>
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      className={`group flex items-center gap-1 self-start pt-2 transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500" 
+      }`}
+    >
+      <div className="p-1">
+        <HeartIcon
+          className={`transition-colors duration-150 ${
+            likedByMe
+              ? "fill-red-500"
+              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500 "
+          }`}
+        />
+      </div>
+      <span>{likeCount}</span>
+    </button>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { type NextPage } from "next";
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import LoadingSpinner, { LoadingPage } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
 import { PostView } from "~/components/postview";
+import { useInView } from "react-intersection-observer";
+
+const TABS = ["Recent", "Following"] as const;
 
 function CreatePostWizard() {
   const { user } = useUser();
@@ -18,7 +22,7 @@ function CreatePostWizard() {
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput("");
-      void ctx.posts.getAll.invalidate();
+      void ctx.posts.infiniteFeed.invalidate();
     },
     onError: (event) => {
       const errorMessage = event.data?.zodError?.fieldErrors.content;
@@ -33,17 +37,13 @@ function CreatePostWizard() {
   if (!user) return null;
 
   return (
-    <div className="flex w-full gap-3">
-      <UserButton
-        appearance={{
-          elements: {
-            // see if you can change on hover classes
-            userButtonAvatarBox: {
-              width: 56,
-              height: 56,
-            },
-          },
-        }}
+    <div className="flex w-full gap-3 bg-black">
+      <Image
+        src={user.profileImageUrl}
+        alt={user.username ? `@${user.username}'s profile picture` : ""}
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -73,42 +73,138 @@ function CreatePostWizard() {
   );
 }
 
-function Feed() {
-  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+function RecentFeed() {
+  const { ref, inView } = useInView();
+
+  const {
+    data,
+    isLoading: postsLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+  } = api.posts.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastpage) => lastpage.nextCursor }
+  );
+
+  const posts = data?.pages.flatMap((post) => post.posts);
 
   if (postsLoading) return <LoadingPage />;
 
   if (!data) return <div>Something went wrong!</div>;
 
+  if (inView && hasNextPage && !isFetching) {
+    void fetchNextPage();
+  }
+
   return (
     <div className="flex flex-col">
-      {data.map((fullPost) => (
-        <PostView {...fullPost} key={fullPost.post.id} />
-      ))}
+      {posts &&
+        posts.map((fullPost) => (
+          <PostView {...fullPost} key={fullPost.post.id} />
+        ))}
+      <span ref={ref} className={hasNextPage ? "invisible" : "hidden"}>
+        intersection observer marker
+      </span>
+      {isFetching && (
+        <div className="pb-2">
+          <LoadingPage />
+        </div>
+      )}
     </div>
+  );
+}
+
+function FollowingFeed() {
+  const { ref, inView } = useInView();
+
+  const {
+    data,
+    isLoading: postsLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+  } = api.posts.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
+    { getNextPageParam: (lastpage) => lastpage.nextCursor }
+  );
+
+  const posts = data?.pages.flatMap((post) => post.posts);
+
+  if (postsLoading) return <LoadingPage />;
+
+  if (!data) return <div>Something went wrong!</div>;
+
+  if (inView && hasNextPage && !isFetching) {
+    void fetchNextPage();
+  }
+
+  return (
+    <div className="flex flex-col">
+      {posts &&
+        posts.map((fullPost) => (
+          <PostView {...fullPost} key={fullPost.post.id} />
+        ))}
+      <span ref={ref} className={hasNextPage ? "invisible" : "hidden"}>
+        intersection observer marker
+      </span>
+      {isFetching && <LoadingPage />}
+    </div>
+  );
+}
+
+function Header() {
+  const handleJumpUpClick = () => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  };
+
+  return (
+    <header
+      className="w-full cursor-pointer bg-black p-4 py-[1.125rem]"
+      onClick={handleJumpUpClick}
+    >
+      <p className="">Home</p>
+    </header>
   );
 }
 
 const Home: NextPage = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
-
-  // start fetching asap
-  api.posts.getAll.useQuery();
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
 
   // return empty div if BOTH aren't loaded, since user tends to load faster
   if (!userLoaded) return <div />;
 
   return (
     <PageLayout>
-      <div className="flex border-b border-slate-400 p-4">
-        {isSignedIn && <CreatePostWizard />}
-        {!isSignedIn && (
-          <div className="flex items-center justify-center">
-            <SignInButton />
+      <div className="sticky top-0">
+        <Header />
+        {isSignedIn && (
+          <div className="flex bg-black">
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow bg-black p-2 hover:bg-slate-700 focus-visible:bg-slate-700 ${
+                    tab === selectedTab ? "border-b-4 border-b-slate-500" : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
           </div>
         )}
       </div>
-      <Feed />
+      {isSignedIn && (
+        <div className="flex border-b border-slate-400 p-4">
+          <CreatePostWizard />
+        </div>
+      )}
+
+      {selectedTab === "Recent" ? <RecentFeed /> : <FollowingFeed />}
     </PageLayout>
   );
 };
