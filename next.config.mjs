d62f4004ac3741e6b16f02fe2a63f538a5d6773b
mechakin <barrientosryan83@gmail.com>
2023-05-31 20:49:03 -0400

    change ci

--- next.config.mjs
+++ next.config.mjs
@@ -21,15 +21,7 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
+  
   swcMinify: true,
 };
 export default config;
